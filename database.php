576612ef8bb1c162f11291816cd113b8f8fae745
Renato Moraes <hexemeister@gmail.com>
2018-02-25 00:54:29 -0300

    implementado ajax e função de busca especifica na tabela

--- database.php
+++ database.php
@@ -19,3 +19,25 @@ function DBRead($table, $fields = '*', $params = NULL){
     }
     DBClose($link);
 }
+
+function DBAtendimentoByDateRangeRead($dataInicio, $dataFim){
+    $link = DBConnect();
+    $query = "SELECT senha, hrinicio, nomedoador, 
+                    hrtermino, nmpacie, tipodoa, 
+                    convert(varchar,dtatend, 103) AS Data , hospital 
+              FROM atendim
+              WHERE dtatend BETWEEN '{$dataInicio}' AND '{$dataFim}' ORDER BY dtatend, senha ";
+    $result = @sqlsrv_query($link, $query, array(), array( "Scrollable" => 'static' )) or die(sqlsrv_errors());;
+
+    if (!sqlsrv_num_rows($result)) {
+        return false;
+    } else {
+        while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC))
+        {
+            $data[] = $row  ;
+        }
+        return $data;
+    }
+    DBClose($link);
+}
+?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
 <head>
 <!-- Required meta tags -->
 <meta charset="utf-8">
@@ -8,31 +8,30 @@
 
 <!-- Bootstrap CSS -->
 <link rel="stylesheet"
-	href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+	href="resources\css\bootstrap.min.css"
 	integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
 	crossorigin="anonymous">
 
 <?php
-require 'config.php';
-require 'connection.php';
-require 'database.php';
-require 'viewUtils.php';
-require 'control.php';
-
-// var_dump(DBRead('atendim','nomedoador, doador', NULL));
+require_once 'config.php';
+require_once 'connection.php';
+require_once 'database.php';
+require_once 'viewUtils.php';
+require_once 'control.php';
 ?>
-
     <title>Monitora Web</title>
 </head>
 <body>
 	<div class="container-fluid">
 		<div class="jumbotron jumbotron-fluid text-center text-top">
 			<div class="container">
-				<h1 class="display-3">Monitora Web</h1>
+				<h1 class="display-5">Monitora Web</h1>
 				<p class="lead">Sistema Web de Monitoramento de Doações</p>
 			</div>
 		</div>
-
+	<?php	foreach(get_included_files() as $a){
+        echo $a."<br>";
+    } ?>
 		<form action="control.php" method="post">
 			<div class="form-row">
 				<div class="col">
@@ -44,44 +43,27 @@ require 'control.php';
 					<input type="date" id="dataFim" name="dataFim" class="form-control" placeholder="Data Final" id = "lastdatepicker" name = "lastdatepicker">
 				</div>
 			</div>
-			<input type="submit" value="Envia" />
+			<button style="margin-top:5px;" id="botao"class="btn btn-primary" type="button">Envia</button>
 		</form>
 	
-	
-<?php
-echo build_table(DBRead('atendim', 'senha, hrinicio, nomedoador, hrtermino, nmpacie, tipodoa, convert(varchar,dtatend, 103) AS Data , hospital', "WHERE dtatend BETWEEN '3/02/2017' AND '13/02/2017' ORDER BY dtatend, senha "));
-?>
+	<div id="table">Tabela Aqui
+		<?php echo build_table(DBAtendimentoByDateRangeRead("13/02/2017", "13/02/2017")); ?>
+	</div>
 </div>
 	<!-- Optional JavaScript -->
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+	<script src="resources\js\jquery-3.2.1.slim.min.js"
 		integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
 		crossorigin="anonymous"></script>
 	<script
-		src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+		src="resources\js\popper.min.js"
 		integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
 		crossorigin="anonymous"></script>
 	<script
-		src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+		src="resources\js\bootstrap.min.js"
 		integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
 		crossorigin="anonymous"></script>
+	<script type="text/javascript" src="resources/js/scripts.js" ></script>
 
-	<script>
-var start = document.getElementById('dataInicio');
-var end = document.getElementById('dataFim');
-
-start.addEventListener('change', function() {
-    if (start.value){
-        end.min = start.value;
-        start.max = end.value;
-	}
-}, false);
-end.addEventListener('change', function() {
-    if (end.value){
-        end.min = start.value;
-        start.max = end.value;
-	}
-}, false);
-  </script>
 </body>
 </html>
